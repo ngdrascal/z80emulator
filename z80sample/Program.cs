bf7aadf03379c95149c9db58e892cd0e2d22e28e
Rick Dodd <rick.dodd@comcast.net>
2022-02-06 10:29:18 -0600

    code cleanup and formatting

--- z80sample/Program.cs
+++ z80sample/Program.cs
@@ -10,8 +10,10 @@ namespace z80Sample
         {
             var ram = new byte[65536];
             Array.Clear(ram, 0, ram.Length);
+
             var inp = File.ReadAllBytes("48.rom");
-            if (inp.Length != 16384) throw new InvalidOperationException("Invalid 48.rom file");
+            if (inp.Length != 16384)
+                throw new InvalidOperationException("Invalid 48.rom file");
 
             Array.Copy(inp, ram, 16384);
 
@@ -31,37 +33,49 @@ namespace z80Sample
                 //}
             }
 
-
             Console.WriteLine(Environment.NewLine + myZ80.DumpState());
             for (var i = 0; i < 0x80; i++)
             {
-                if (i % 16 == 0) Console.Write("{0:X4} | ", i);
+                if (i % 16 == 0) 
+                    Console.Write("{0:X4} | ", i);
+
                 Console.Write("{0:x2} ", ram[i]);
-                if (i % 8 == 7) Console.Write("  ");
-                if (i % 16 == 15) Console.WriteLine();
+                
+                if (i % 8 == 7) 
+                    Console.Write("  ");
+                
+                if (i % 16 == 15) 
+                    Console.WriteLine();
             }
             Console.WriteLine();
             for (var i = 0x4000; i < 0x4100; i++)
             {
-                if (i % 16 == 0) Console.Write("{0:X4} | ", i);
+                if (i % 16 == 0) 
+                    Console.Write("{0:X4} | ", i);
+
                 Console.Write("{0:x2} ", ram[i]);
-                if (i % 8 == 7) Console.Write("  ");
-                if (i % 16 == 15) Console.WriteLine();
+                if (i % 8 == 7) 
+                    Console.Write("  ");
+
+                if (i % 16 == 15) 
+                    Console.WriteLine();
             }
         }
     }
 
-    class SamplePorts : IPorts
+    internal class SamplePorts : IPorts
     {
         public byte ReadPort(ushort port)
         {
             Console.WriteLine($"IN 0x{port:X4}");
             return 0;
         }
+
         public void WritePort(ushort port, byte value)
         {
             Console.WriteLine($"OUT 0x{port:X4}, 0x{value:X2}");
         }
+
         public bool NMI => false;
         public bool MI => false;
         public byte Data => 0x00;
